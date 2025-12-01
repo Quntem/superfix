a84b557edf31ece4cd4d8e29d2559a9f909d6b6d
Sabio <gamester.sad@gmail.com>
2025-12-01 21:22:13 +0000

    Quntem Labs redirection

--- components/header.tsx
+++ components/header.tsx
@@ -6,7 +6,7 @@ export default function Header() {
       <Logo/>
       <div className="flex flex-row gap-2 items-end">
         <p className="font-bold text-3xl">Superfix</p>
-        <p className="mb-0.5 text-sm">by Quntem Labs</p>
+        <p className="mb-0.5 text-sm">by <a href="https://quntemlabs.org/">Quntem Labs</a></p>
       </div>
     </header>
   )
